ae4b8a01d000996d82d17332b8df65ea3a022739
caio-vinicius <contato@caiovinicius.dev.br>
2022-02-12 11:43:22 -0300

    ex00: initial files

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,14 @@
+/* Copyright (c) 2022 Caio Souza. All rights reserved. */
+/* 42 */
+
+#ifndef FIXED_H
+#define FIXED_H
+
+class Fixed {
+ private:
+
+ public:
+    
+};
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,8 @@
+/* Copyright (c) 2022 Caio Souza. All rights reserved. */
+/* 42 */
+
+#include "Fixed.hpp"
+
+int main(void) {
+    return (0);
+}
